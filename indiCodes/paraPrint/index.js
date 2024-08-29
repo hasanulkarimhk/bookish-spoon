5f22020962c968cd59a5226d401fcf460b04c4bc
hasanulkarimhk <hasanulkarimhk@yahoo.com>
2024-08-29 17:55:08 +0600

    555

---
+++ indiCodes/paraPrint/index.js
@@ -0,0 +1,7 @@
+function takeValue() {
+  document.querySelector('.txt0').value;
+}
+
+function printFun() {
+  document.querySelector('.herePut').innerHTML = document.querySelector('.txt0').value;
+}

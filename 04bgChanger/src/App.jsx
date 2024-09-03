a946cc298dddec80d351517b22b5262555bd80ae
hasanulkarimhk <hasanulkarimhk@yahoo.com>
2024-09-03 23:04:10 +0600

    complete gg

--- 04bgChanger/src/App.jsx
+++ 04bgChanger/src/App.jsx
@@ -10,16 +10,24 @@ function App() {
       <div className="fixed flex flex-wrap justify-center bottom-12 inset-x-0 px-2">
         <div className="flex flex-wrap justify-center gap-3 shadow-lg bg-white px-3 py-2 rounded-lg">
           <button
-          onClick={{} => setColor}
-           className="outline-none px-4 py-1 rounded-lg shadow-md text-white"
+            onClick={() => setColor("red")}
+            className="outline-none px-4 py-1 rounded-lg shadow-md text-white"
             style={{ backgroundColor: "red" }}>Red</button>
-          <button className="outline-none px-4 py-1 rounded-lg shadow-md text-white"
+          <button
+            onClick={() => setColor("#87FF65")}
+            className="outline-none px-4 py-1 rounded-lg shadow-md text-white"
             style={{ backgroundColor: "#87FF65" }}>Green</button>
-          <button className="outline-none px-4 py-1 rounded-lg shadow-md text-white"
+          <button
+            onClick={() => setColor("#7EB2DD")}
+            className="outline-none px-4 py-1 rounded-lg shadow-md text-white"
             style={{ backgroundColor: "#7EB2DD" }}>Calorina Blue</button>
-          <button className="outline-none px-4 py-1 rounded-lg shadow-md text-white"
+          <button
+            onClick={() => setColor("#011936")}
+            className="outline-none px-4 py-1 rounded-lg shadow-md text-white"
             style={{ backgroundColor: "#011936" }}>OX Blue</button>
-          <button className="outline-none px-4 py-1 rounded-lg shadow-md text-white"
+          <button
+            onClick={() => setColor("#4A306D")}
+            className="outline-none px-4 py-1 rounded-lg shadow-md text-white"
             style={{ backgroundColor: "#4A306D" }}>Tekhelet</button>
 
         </div>

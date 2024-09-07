0a3d89633d0b787349c944d6874fc7f71405a189
hasanulkarimhk <hasanulkarimhk@yahoo.com>
2024-09-07 22:55:40 +0600

    um done

--- 05passChanger/src/App.jsx
+++ 05passChanger/src/App.jsx
@@ -11,41 +11,73 @@ function App() {
     let pass = ''
     let str = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz'
 
-    if(numberAllowed) str += "0123456789"
-    if(charPositive) str += "!@#$%^&*-_+=[]{}`"
+    if (numberAllowed) str += "0123456789"
+    if (charPositive) str += "!@#$%^&*-_+=[]{}`"
 
-    for (let i = 1 ; i <= array.length; i++){
+    for (let i = 1; i <= length; i++) {
       let char = Math.floor(Math.random() * str.length + 1)
 
       pass = str.charAt(char)
     }
     setPass(pass)
-  }, [length,numberAllowed,charPositive, pass])
+  }, [length, numberAllowed, charPositive, pass])
 
   return (
     <>
-    <div className="w-full max-w-md mx-auto shawdow-md rounded-lg px-4 my-8 text-orange-500 bg-gray-800 justify-center">
+      <div className="w-full max-w-md mx-auto shawdow-md rounded-lg px-4 my-8 text-orange-500 bg-gray-800 justify-center">
 
-    <div className="flex shadow rounded-lg overflow-hiddden mb-4">
+        <div className="flex shadow rounded-lg overflow-hiddden mb-4">
 
-      <input 
-      value={pass}
-      className="outline-none w-full py-1 px-3"
-      placeholder="password"
-      readOnly
-      type="text" />
-      <button className="outline-none justify-center border-spacing-3 text-wrap text-center">copy</button>
-
-
+          <input
+            value={pass}
+            className="outline-none w-full py-1 px-3"
+            placeholder="password"
+            readOnly
+            type="text" />
+          <button className="outline-none justify-center border-spacing-3 text-wrap text-center">copy</button>
 
 
-    </div>
-    <div className="flex"></div>
-    <div className="flex"></div>
-    <input type="range" min={6} max={100} 
-    value={length} className="cursor-pointer"/>
 
-    </div>
+
+        </div>
+        <div className="flex"></div>
+        <div className="flex"></div>
+        <input type="range" min={8} max={100}
+          value={length}
+          className="cursor-pointer"
+          onChange={(e) => { setLength(e.target.value) }}
+        />
+        <label>Length: {length}</label>
+
+        <div className="flex items-center gap-x-1">
+          <input type="checkbox"
+            defaultChecked={numberAllowed}
+            id="numberInput"
+            onChange={() => {
+              setNA((prev) => !prev);
+            }}
+
+          />
+          <label htmlFor="numberInput" className="text-white">Numbers</label>
+        </div>
+        <div className="flex items-center gap-x-1">
+          <input type="checkbox"
+            defaultChecked={charPositive}
+            id="numberInput"
+            onChange={() => {
+              setCharPositive((prev) => !prev);
+            }}
+
+          />
+          <label htmlFor="numberInput" className="text-white">Characters</label>
+        </div>
+
+      </div>
+
+
+
+
+
     </>
   )
 }

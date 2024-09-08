38767fb001a6931ffc74f9bf1af376af52ff921c
hasanulkarimhk <hasanulkarimhk@yahoo.com>
2024-09-08 17:20:43 +0600

    saving

---
+++ HardpixelTV/src/assets/components/Header.jsx
@@ -0,0 +1,12 @@
+
+function Header() {
+
+  return (
+   <div className="bg-green-111">
+    <h1 className="bg-black text-zinc-100 font-title text-xl w-fit">Hard<span className="font-pixel text-3xl text-green-111">pixel</span> TV</h1>
+   </div>
+  )
+}
+
+export default Header
+// 5465FF 788BFF 9BB1FF BFD7FF E2FDFF

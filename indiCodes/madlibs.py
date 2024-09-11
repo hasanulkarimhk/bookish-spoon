6b6c0e88a9561b5fcb53e5516300063e4785073f
hasanulkarimhk <hasanulkarimhk@yahoo.com>
2024-09-12 00:46:48 +0600

    that's it

---
+++ indiCodes/madlibs.py
@@ -0,0 +1,22 @@
+# string concatination( as how to put strings together)
+# suppose we want to 
+
+# youtuber = "rititi"
+
+# print("subscribe to " + youtuber)
+
+# print("subscribe to {}".format(youtuber))
+
+# print(f"subscribe to {youtuber}")
+
+
+
+adj = input("Adjective: ")
+verb1 = input("verb1: ")
+verb2 = input("verb2: ")
+famous_person = input("Famous Person: ")
+
+madlib = f"computer proggramming is so {adj}! it makes me so excited all the time because \
+    I love to {verb1}. Stay hydrated and {verb2} like you are {famous_person}"
+
+print(madlib)
--- indiCodes/trial.py
+++
@@ -1,4 +0,0 @@
-if (-1 > 0):
-  print("Hell yeahhh!")
-else:
-  print("Nope Brother!")
